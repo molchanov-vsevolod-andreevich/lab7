e815dec35fe3917dc6e94315d49adeec7cdd9053
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-28 09:51:57 +0300

    Scripted auto-commit on change (2019-12-28 09:51:57) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,16 +1,18 @@
 import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
         ZMQ.Context context = ZMQ.context(1);
         // Socket to talk to server
         ZMQ.Socket requester = context.socket(SocketType.REQ); requester.connect("tcp://localhost:5559"); System.out.println("launch and connect client.");
-        for (int request_nbr = 0; request_nbr < 10; request_nbr++) {
-            requester.send("Hello", 0);
-            String reply = requester.recvStr (0);
-            System.out.println("Received reply " + request_nbr + " [" + reply + "]");
-        }
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Client has been launched. Enter messages");
+
         // We never get here but clean up anyhow requester.close();
         context.term();
     }
