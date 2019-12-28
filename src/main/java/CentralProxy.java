8d5824ed56d8190073a0c10a152838368b572962
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-28 12:06:17 +0300

    Scripted auto-commit on change (2019-12-28 12:06:17) by gitwatch.sh

--- src/main/java/CentralProxy.java
+++ src/main/java/CentralProxy.java
@@ -1,13 +1,20 @@
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+
 public class CentralProxy {
     public static void main(String[] args) {
-        Context context = ZMQ.context(1);
-        Socket frontend = context.socket(SocketType.ROUTER);
-        Socket backend = context.socket(SocketType.DEALER);
-        frontend.bind("tcp://*:5559"); backend.bind("tcp://*:5560");
+        ZMQ.Context context = ZMQ.context(1);
+        ZMQ.Socket frontend = context.socket(SocketType.ROUTER);
+        ZMQ.Socket backend = context.socket(SocketType.DEALER);
+        frontend.bind("tcp://*:5559");
+        backend.bind("tcp://*:5560");
         System.out.println("launch and connect broker.");
 
         // Initialize poll set
-        Poller items = context.poller (2); items.register(frontend, Poller.POLLIN); items.register(backend, Poller.POLLIN); boolean more = false;
+        ZMQ.Poller items = context.poller (2);
+        items.register(frontend, ZMQ.Poller.POLLIN);
+        items.register(backend, ZMQ.Poller.POLLIN);
+        boolean more = false;
         byte[] message;
 
         // Switch messages between sockets
@@ -25,11 +32,13 @@ public class CentralProxy {
             } }
             if (items.pollin(1)) {
                 while (true) {
-                message = backend.recv(0);
-                more = backend.hasReceiveMore(); frontend.send(message, more ? ZMQ.SNDMORE : 0);
-                if(!more){
-                    break;
+                    message = backend.recv(0);
+                    more = backend.hasReceiveMore();
+                    frontend.send(message, more ? ZMQ.SNDMORE : 0);
+                    if(!more){
+                        break;
+                    }
                 }
-            } }
+            }
         }
 }
