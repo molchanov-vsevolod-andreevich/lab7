b1b17e3e9b75d804fe5ece070be0e3cc15039f91
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-28 10:00:59 +0300

    Scripted auto-commit on change (2019-12-28 10:00:59) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -20,7 +20,7 @@ public class Client {
                 break;
             }
 
-            
+            requester.
         }
 
         // We never get here but clean up anyhow requester.close();
