8c65babf16ac2bb1732661e4cd49eaf29f6072d0
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-28 11:01:13 +0300

    Scripted auto-commit on change (2019-12-28 11:01:13) by gitwatch.sh

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,23 +1,27 @@
 import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 
+import java.util.HashMap;
+
 public class Storage {
     public static void main(String[] args) throws InterruptedException {
+        Map<int, String> storage = new HashMap<>();
+        
         ZMQ.Context context = ZMQ.context (1);
 
         // Socket to talk to server
-        ZMQ.Socket storage = context.socket (SocketType.REP);
-        storage.connect ("tcp://localhost:5560");
+        ZMQ.Socket notif = context.socket (SocketType.REP);
+        notif.connect ("tcp://localhost:5560");
 
         while (!Thread.currentThread ().isInterrupted ()) {
         // Wait for next request from client
-            String string = storage.recvStr (0); System.out.printf ("Received request: [%s]\n", string); // Do some 'work'
+            String string = notif.recvStr (0); System.out.printf ("Received request: [%s]\n", string); // Do some 'work'
             Thread.sleep (1000);
         // Send reply back to client
-            storage.send ("World");
+            notif.send ("World");
         }
-        // We never get here but clean up anyhow storage.close();
-        storage.close();
+        // We never get here but clean up anyhow notif.close();
+        notif.close();
         context.term();
     }
 }
