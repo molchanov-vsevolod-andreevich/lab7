f740f0b7d45ca786228b3016e24160c8e5cc694b
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-28 10:41:09 +0300

    Scripted auto-commit on change (2019-12-28 10:41:09) by gitwatch.sh

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -2,7 +2,8 @@ public class Storage {
     public static void main(String[] args) {
         Context context = ZMQ.context (1);
         // Socket to talk to server
-        Socket responder = context.socket (SocketType.REP); responder.connect ("tcp://localhost:5560");
+        Socket responder = context.socket (SocketType.REP);
+        responder.connect ("tcp://localhost:5560");
         while (!Thread.currentThread ().isInterrupted ()) {
         // Wait for next request from client
             String string = responder.recvStr (0); System.out.printf ("Received request: [%s]\n", string); // Do some 'work'
