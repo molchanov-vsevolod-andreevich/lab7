7badbfc526c32fb118c6fcbdb314a02fbac8db55
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-28 09:42:56 +0300

    Scripted auto-commit on change (2019-12-28 09:42:56) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,14 +1,14 @@
 public class Client {
     public static void main(String[] args) {
         Context context = ZMQ.context(1);
-// Socket to talk to server
+        // Socket to talk to server
         Socket requester = context.socket(SocketType.REQ); requester.connect("tcp://localhost:5559"); System.out.println("launch and connect client.");
         for (int request_nbr = 0; request_nbr < 10; request_nbr++) {
             requester.send("Hello", 0);
             String reply = requester.recvStr (0);
             System.out.println("Received reply " + request_nbr + " [" + reply + "]");
         }
-// We never get here but clean up anyhow requester.close();
+        // We never get here but clean up anyhow requester.close();
         context.term();
     }
 }
