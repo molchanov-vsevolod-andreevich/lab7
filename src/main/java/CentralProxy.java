c601b53fc6508ebbb4efab0371b37e4d46a6b7aa
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-28 12:03:49 +0300

    Scripted auto-commit on change (2019-12-28 12:03:49) by gitwatch.sh

--- src/main/java/CentralProxy.java
+++ src/main/java/CentralProxy.java
@@ -5,17 +5,22 @@ public class CentralProxy {
         Socket backend = context.socket(SocketType.DEALER);
         frontend.bind("tcp://*:5559"); backend.bind("tcp://*:5560");
         System.out.println("launch and connect broker.");
-// Initialize poll set
+
+        // Initialize poll set
         Poller items = context.poller (2); items.register(frontend, Poller.POLLIN); items.register(backend, Poller.POLLIN); boolean more = false;
         byte[] message;
-        
+
         // Switch messages between sockets
         while (!Thread.currentThread().isInterrupted()) {
             // poll and memorize multipart detection items.poll();
-            if (items.pollin(0)) { while (true) {
-                message = frontend.recv(0);
-                more = frontend.hasReceiveMore(); backend.send(message, more ? ZMQ.SNDMORE : 0); if(!more){
-                    break; }
+            if (items.pollin(0)) {
+                while (true) {
+                    message = frontend.recv(0);
+                    more = frontend.hasReceiveMore(); backend.send(message, more ? ZMQ.SNDMORE : 0);
+                    if(!more){
+                        break;
+                    }
+                }
             } }
             if (items.pollin(1)) { while (true) {
                 message = backend.recv(0);
