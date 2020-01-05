75701503d674dd166e5043734328230777b17c36
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2020-01-05 18:00:17 +0400

    Scripted auto-commit on change (2020-01-05 18:00:17) by gitwatch.sh

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,8 +1,10 @@
 public class Command {
 
+    private static final int INVALID_COMMAND_TYPE = 0;
     private static final int NOTIFY_COMMAND_TYPE = 1;
-    private static final int _COMMAND_TYPE = 1;
-    private static final int NOTIFY_COMMAND_TYPE = 1;
+    private static final int PUT_COMMAND_TYPE = 2;
+    private static final int GET_COMMAND_TYPE = 3;
+    private static final int EXIT_COMMAND_TYPE = 4;
 
     private int commandType;
     private String args;
@@ -12,7 +14,7 @@ public class Command {
         String commandName = splittedCmd[0];
         switch (commandName) {
             case "NOTIFY":
-                commandType =
+                commandType = 1;
         }
     }
 
