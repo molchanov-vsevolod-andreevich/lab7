8344db2d981e74220549c1823046a84203655336
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2020-01-05 17:56:43 +0400

    Scripted auto-commit on change (2020-01-05 17:56:43) by gitwatch.sh

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,10 +1,22 @@
 public class Command {
 
-    int commandType;
-    String args;
+    private int commandType;
+    private String args;
 
     public Command(String cmd) {
         String[] splittedCmd = cmd.split(" ");
-        
+        String commandName = splittedCmd[0];
+        switch (commandName) {
+            case "NOTIFY":
+                
+        }
+    }
+
+    public int getCommandType() {
+        return commandType;
+    }
+
+    public String getArgs() {
+        return args;
     }
 }
