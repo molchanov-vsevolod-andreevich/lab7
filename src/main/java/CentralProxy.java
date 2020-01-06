d3a5f73e7b8dbaafcae3120d7a9f4f9025e8dd41
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2020-01-07 03:50:00 +0400

    Scripted auto-commit on change (2020-01-07 03:50:00) by gitwatch.sh

--- src/main/java/CentralProxy.java
+++ src/main/java/CentralProxy.java
@@ -80,6 +80,7 @@ public class CentralProxy {
 
     private static void processNotifyMsg(Command command, ZMsg msg) {
         ZFrame storageID = msg.unwrap();
+        System.out.println(command.prettyPrinting());
         storages.putIfAbsent(storageID, new StorageInfo(command.getArgs()));
         storages.get(storageID).setLastNotificationTime(System.currentTimeMillis());
     }
